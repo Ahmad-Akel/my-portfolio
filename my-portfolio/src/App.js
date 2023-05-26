4b02685a2c85df1abc8345ac862a10a5caebf6fd
Ahmad Akel <ahmad.akel@mgm-tp.com>
2023-05-27 01:38:46 +0200

    Setting Up The Project

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experiences from "./pages/Experiences";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experiences" element={<Experiences />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
