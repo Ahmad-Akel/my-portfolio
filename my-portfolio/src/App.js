afc29654902146a9354b21f6d00bf9eb5f6316c7
Ahmad Akel <ahmad.akel@mgm-tp.com>
2023-05-27 13:25:09 +0200

    Finishing The Projects Page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import "./styles/Navbar.css";
 import "./styles/App.css";
 import Footer from "./components/Footer";
+import ProjectDisplay from "./pages/ProjectDisplay";
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experiences" element={<Experiences />} />
         </Routes>
         <Footer />
---
+++ my-portfolio/src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from "../assets/project1.jpg";
+import Proj2 from "../assets/project2.jpg";
+import Proj3 from "../assets/project3.jpg";
+import Proj4 from "../assets/project4.jpg";
+import Proj5 from "../assets/project5.jpg";
+import Proj6 from "../assets/project6.jpg";
+
+export const ProjectList = [
+  {
+    name: "Dijkstra Algorithm Visualizer",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Ecommerce Website",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB",
+  },
+  {
+    name: "Spotify Clone",
+    image: Proj3,
+    skills: "React,Node.js,MongoDB,SpotifyAPI",
+  },
+  {
+    name: "Social Media Website",
+    image: Proj4,
+    skills: "React,Node.js,MySQL,GraphQL",
+  },
+  {
+    name: "Dashboard Visualizer",
+    image: Proj5,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Mobile Game",
+    image: Proj6,
+    skills: "React Native,JavaScript,HTML,CSS",
+  },
+];
